4379f5ef4a9502040df631b764c79902c2fd6f7e
tobihans <learningway403@gmail.com>
2022-10-08 22:35:36 +0100

    essay version

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Setup.sh
+# Configure my new machines and gitpod environments
+
+source tools.sh
+
+if is::gitpod
+  install::omb
+  install::neovim
+  install::astro
+  install::chezmoi
+else
+  install::omb
+  install::chezmoi
+  install::sdk
+  install::nvm
+  install::rustup
+  install::neovim
+  install::astro
+fi
