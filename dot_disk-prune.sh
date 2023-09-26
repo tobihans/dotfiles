0eb261a0958c95cfe2990dfd9465d1b2d1e8ec3b
Hans Tognon <learningway403@gmail.com>
2023-09-26 09:58:41 +0100

    feat: scripts to clear some cache directories

---
+++ dot_disk-prune.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# This script is used to prune language package managers cache
+# and the assets they produce.
+
+# Node
+npm cache clean --force
+pnpm store prune
+yarn cache clean
+find ./Code -name "node_modules" -type d -prune -exec rm -rf '{}' +
+find ./github -name "node_modules" -type d -prune -exec rm -rf '{}' +
+
+# Python
+pip cache purge
+poetry cache clear --all -n PyPI
+poetry cache clear --all -n _default_cache
+
+# TODO: Add fr Cargo once I have a better understading of how it works
+
+# Go
+go clean -modcache
+
+# System
+yay -Sc --noconfirm
