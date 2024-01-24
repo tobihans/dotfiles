8ba380dfae49fbdbcbccda98fee3387c311ff553
Hans Tognon <git@tobihans.space>
2024-01-24 10:47:25 +0100

    chore: lazy imports update

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -53,8 +53,6 @@ lazy.setup({
   -- Packs
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.zig" }, -- Hehe, let's have some fun
-  { import = "astrocommunity.pack.nim" }, -- Hehe, let's have some fun
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.java" },
