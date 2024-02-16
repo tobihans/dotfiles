d80b53b6a52bc319067acd7076ee5441a911a02c
Hans Tognon <git@tobihans.space>
2024-02-16 15:42:43 +0100

    feat: setup act

--- setup.sh
+++ setup.sh
@@ -128,6 +128,9 @@ function install::cli_tools() {
 	python -m pipx inject --include-apps ansible ansible-lint
 	printf "Instructions for sqlite-utils:\n"
 	printf "sqlite-utils install sqlean.py\nsqlite-utils install sqlite-dump"
+
+	# gh exynos
+	gh extension install https://github.com/nektos/gh-act
 }
 
 function setup() {
