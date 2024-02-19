e014a41e356f92a0611f8e9b1d890c75794defcb
Hans Tognon <git@tobihans.space>
2024-02-19 12:23:58 +0100

    fix: add a post_load hook to resession

--- dot_config/nvim/lua/plugins/resession.lua
+++ dot_config/nvim/lua/plugins/resession.lua
@@ -1,18 +1,23 @@
 ---@type LazySpec
 return {
-  {
-    "stevearc/resession.nvim",
-    opts = function(_, opts)
-      opts.extensions = {
-        astrocore = {},
-        quickfix = {},
-        scope = {},
-        aerial = {},
-        overseer = {
-          unique = true,
-          recent_first = true,
-        },
-      }
-    end,
-  },
+  "stevearc/resession.nvim",
+  opts = function(_, opts)
+    opts.extensions = {
+      astrocore = {},
+      quickfix = {},
+      scope = {},
+      aerial = {},
+      overseer = {
+        unique = true,
+        recent_first = true,
+      },
+    }
+  end,
+  config = function(_, opts)
+    local resession = require "resession"
+    resession.setup(opts)
+
+    -- NOTE: Highliting doesn't work on the current window when loading a session.
+    resession.add_hook("post_load", function() vim.cmd [[edit %]] end)
+  end,
 }
