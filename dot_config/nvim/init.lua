7078ea28f130432fa368008501f77e4fdedf71a8
Hans Tognon <git@tobihans.space>
2024-03-22 12:30:31 +0100

    chore: updates

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -5,6 +5,7 @@ if not (vim.env.LAZY or vim.loop.fs_stat(lazypath)) then
   -- stylua: ignore
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
+---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 if not pcall(require, "lazy") then
