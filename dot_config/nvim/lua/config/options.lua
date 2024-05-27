ccec39682ad7776bf1757e107b19a96cde29490f
Hans Bignon K. Tognon <64963758+tobihans@users.noreply.github.com>
2024-05-27 18:30:45 +0100

    Update options.lua

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -102,7 +102,7 @@ vim.api.nvim_exec2(
 if vim.fn.has "win32" == 1 then
     vim.cmd([[
 let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
-let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
+let &shellcmdflag = '-NoLogo -NoProfileLoadTime -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
 let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
 let &shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
 set shellquote= shellxquote=
