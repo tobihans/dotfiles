752be71f257802048b282ed8fc40bdfbb745099e
Hans B. K. Tognon <git@tobihans.space>
2024-08-26 09:43:30 +0100

    chore: savepoint

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -5,10 +5,34 @@ return {
     source_selector = {
       winbar = false,
     },
-    -- filesystem = {
-    --   filtered_items = {
-    --     visible = true,
-    --   },
-    -- },
+    filesystem = {
+      -- filtered_items = {
+      --   visible = true,
+      -- },
+      commands = {
+        delete = function(state)
+          local inputs = require "neo-tree.ui.inputs"
+          local path = state.tree:get_node().path
+          local msg = string.format("Are you sure you want to trash '%s'?", path)
+          inputs.confirm(msg, function(confirmed)
+            if confirmed then
+              vim.system({ "trash-put", "-f", path }, {}):wait()
+              require("neo-tree.sources.manager").refresh(state.name)
+            end
+          end)
+        end,
+        delete_visual = function(state, selected_nodes)
+          local inputs = require "neo-tree.ui.inputs"
+          local msg = string.format("Are you sure you want to trash %d items?", #selected_nodes)
+          inputs.confirm(msg, function(confirmed)
+            if confirmed then
+              local paths = vim.iter(selected_nodes):map(function(n) return n.path end):totable()
+              vim.system({ "trash-put", "-f", unpack(paths) }, {}):wait()
+              require("neo-tree.sources.manager").refresh(state.name)
+            end
+          end)
+        end,
+      },
+    },
   },
 }
