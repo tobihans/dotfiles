dd743557943935a0a7512e45f2272dd949b2a36a
Hans B. K. Tognon <git@tobihans.space>
2025-11-11 10:51:32 +0100

    fix(nvim): use trash for delete on mac

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -1,3 +1,7 @@
+local TRASH_CMD = { "trash-put", "-f" }
+
+if vim.uv.os_uname().sysname == "Darwin" then TRASH_CMD = { "trash", "-s" } end
+
 ---@type LazySpec
 return {
   "nvim-neo-tree/neo-tree.nvim",
@@ -19,7 +23,7 @@ return {
           local msg = string.format("Are you sure you want to trash '%s'?", path)
           inputs.confirm(msg, function(confirmed)
             if confirmed then
-              vim.system({ "trash-put", "-f", path }, {}):wait()
+              vim.system({ unpack(TRASH_CMD), path }, {}):wait()
               require("neo-tree.sources.manager").refresh(state.name)
             end
           end)
@@ -30,7 +34,7 @@ return {
           inputs.confirm(msg, function(confirmed)
             if confirmed then
               local paths = vim.iter(selected_nodes):map(function(n) return n.path end):totable()
-              vim.system({ "trash-put", "-f", unpack(paths) }, {}):wait()
+              vim.system({ unpack(TRASH_CMD), unpack(paths) }, {}):wait()
               require("neo-tree.sources.manager").refresh(state.name)
             end
           end)
