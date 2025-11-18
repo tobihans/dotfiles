75a6eafed4711214d2ad6c27edacc6ab3489e183
Hans B. K. Tognon <git@tobihans.space>
2025-11-18 11:23:00 +0100

    feat(nvim): remove unneeded file with overseer update

--- dot_config/nvim/lua/plugins/overseer.lua
+++ dot_config/nvim/lua/plugins/overseer.lua
@@ -17,27 +17,17 @@ return {
       "OverseerToggle",
     },
     keys = {
-      { "<Leader>or", "<cmd>OverseerRun<cr>", desc = " Run Task" },
+      { "<Leader>or", "<cmd>OverseerRun<cr>",    desc = " Run Task" },
       { "<Leader>ot", "<cmd>OverseerToggle<CR>", desc = " Toggle tasks" },
     },
     opts = {
-      strategy = {
-        "toggleterm",
-        use_shell = false,
-        direction = "horizontal",
-        hidden = false,
-        auto_scroll = true,
-        open_on_start = true,
-        quit_on_exit = "success",
-        on_create = function(terminal) terminal.display_name = string.format("%s", terminal.cmd) end,
-      },
-      templates = { "builtin", "custom" },
+      templates_dirs = { "overseer" },
       task_list = {
         direction = "right",
       },
       component_aliases = {
         default = {
-          { "display_duration", detail_level = 2 },
+          { "display_duration",    detail_level = 2 },
           "on_output_summarize",
           "on_exit_set_status",
           "on_complete_notify",
