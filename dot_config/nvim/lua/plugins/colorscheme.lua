e66c8f71508e1a4e837e070131c763a4b63424fe
Hans B. K. Tognon <git@tobihans.space>
2025-12-30 14:19:25 +0000

    feat(nvim): switch to onedark theme

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,24 +1,35 @@
 ---@type LazySpec
 return {
+  -- {
+  --   "webhooked/kanso.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {
+  --     background = {
+  --       dark = "ink",
+  --       light = "pearl",
+  --     },
+  --     overrides = function(_colors)
+  --       return {
+  --         WinSeparator = { link = "FloatBorder" },
+  --       }
+  --     end,
+  --   },
+  -- },
+
+  -- https://github.com/everviolet/nvim
+
   {
-    "webhooked/kanso.nvim",
+    "navarasu/onedark.nvim",
     lazy = false,
     priority = 1000,
     opts = {
-      background = {
-        dark = "ink",
-        light = "pearl",
-      },
-      overrides = function(_colors)
-        return {
-          WinSeparator = { link = "FloatBorder" },
-        }
-      end,
+      style = "warm",
     },
   },
   {
     "AstroNvim/astroui",
     ---@type AstroUIOpts
-    opts = { colorscheme = "kanso" },
+    opts = { colorscheme = "onedark" },
   },
 }
